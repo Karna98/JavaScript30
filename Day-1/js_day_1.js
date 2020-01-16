eefb6f8d8ac506e99b19c7ba9a041e2ac57641fd
karna98 <vedantwakalkar@gmail.com>
2020-01-16 21:00:18 +0530

    Day-1

---
+++ Day-1/js_day_1.js
@@ -0,0 +1,12 @@
+function playSound(e) {
+	const audio = document.querySelector(`audio[data-key = '${e.keyCode}']`);
+	const button = document.querySelector(`.button[data-key = '${e.keyCode}']`);
+	if (!audio) return;
+	audio.currentTime = 0;
+	audio.play();
+	button.classList.add("playing");
+	setTimeout(function() {
+		button.classList.remove("playing");
+	}, audio.duration * 1000);
+}
+window.addEventListener("keydown", playSound);
