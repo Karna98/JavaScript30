4cd354a93135d260e580eaddaf270cfbc9213c05
karna98 <vedantwakalkar@gmail.com>
2021-03-22 00:57:16 +0530

    UI added

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function index() {
 	const anchors = document.querySelectorAll("a");
+	const divContainer = document.getElementsByClassName("container");
+
+	var node = [];
+	for (let day = 1; day < 31; day++) {
+		var dayDiv = document.createElement("a");
+		dayDiv.setAttribute('id','Day-' + day);
+		dayDiv.setAttribute('class','item');
+		dayDiv.setAttribute('target','_blank');
+		dayDiv.setAttribute('aria-hiddent','true');
+		dayDiv.setAttribute('href','/Day-' + day + '/html_day_' + day + '.html');
+		dayDiv.innerHTML = "Day " + day;
+		divContainer[0].appendChild(dayDiv);
+	}
 
 	function redirectTo(id) {
 		urlId = id.substring(4);
