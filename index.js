aa96672584dd753b30e60e135aefb4b6b9d0dc2f
karna98 <vedantwakalkar@gmail.com>
2021-03-22 01:01:05 +0530

    Fixed dynamic URL

--- index.js
+++ index.js
@@ -9,28 +9,9 @@ function index() {
 		dayDiv.setAttribute('class','item');
 		dayDiv.setAttribute('target','_blank');
 		dayDiv.setAttribute('aria-hiddent','true');
-		dayDiv.setAttribute('href','/Day-' + day + '/html_day_' + day + '.html');
+		dayDiv.setAttribute('href','/JavaScript30/Day-' + day + '/html_day_' + day + '.html');
 		dayDiv.innerHTML = "Day " + day;
 		divContainer[0].appendChild(dayDiv);
 	}
-
-	function redirectTo(id) {
-		urlId = id.substring(4);
-		if (urlId <= 30) {
-			url =
-				"https://karna98.github.io/JavaScript30/Day-" +
-				urlId +
-				"/html_day_" +
-				urlId +
-				".html";
-			anchorElement = document.querySelectorAll("a").item(urlId - 1);
-			anchorElement.href = url;
-		}
-	}
-	anchors.forEach(anchor =>
-		anchor.addEventListener("onclick", function(e) {
-			redirectTo(id);
-		})
-	);
 }
 window.addEventListener("DOMContentLoaded", index);
